ea8154a0fac16bf28d34ccc4e4f0abbb2a4f8933
Ascani Carlo <carlo.ratm@gmail.com>
2020-12-06 12:06:41 +0100

    Add some new styles

--- serum.exs
+++ serum.exs
@@ -1,6 +1,7 @@
 %{
-  site_name: "Carlo Ascani personal website.",
-  site_description: "The personal website of Carlo Ascani, a web developer.",
+  site_name: "The personal website of Carlo Ascani, a web developer in Italy.",
+  site_description:
+    "The personal website of Carlo Ascani, aka harloratm, a web developer located in Pistoia, Italy.",
   date_format: "{WDfull}, {D} {Mshort} {YYYY}",
   base_url: "/",
   author: "Carlo Ascani",
